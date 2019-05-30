9d5b457e8ce20abbcf4473ba1765c1168760cf1d
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-05-30 11:00:37 +0200

    Improve scripts

--- docker-images/reverse-proxy/run.sh
+++ docker-images/reverse-proxy/run.sh
@@ -1,3 +1,26 @@
+#! /bin/bash
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 docker run --rm -d --name res-php res/php
 docker run -d --name res-express --rm res/express
+
+path=$(realpath "${0%/*}")
+
+echo $path
+
+if [[ "$(docker images -q res/reverse-proxy 2> /dev/null)" == "" ]]
+then
+    echo "Image not found. Building it!"
+    docker build --tag res/reverse-proxy $path
+fi
+
+if [[ "$(docker ps -f name="res-reverse-proxy" -q 2>/dev/null)" != "" ]]
+then
+    echo "Running container found. Stopping it!"
+    docker stop res-reverse-proxy &>/dev/null
+fi
+
+echo "Running image \"res/reverse-proxy\"."
 docker run --rm -p 8080:80 --name res-reverse-proxy -d res/reverse-proxy
--- docker-images/reverse-proxy/stop.sh
+++ docker-images/reverse-proxy/stop.sh
@@ -1,3 +1,19 @@
-docker stop res-reverse-proxy
-docker stop res-php
-docker stop res-express
+#!/bin/bash
+
+if [[ "$(docker ps -f name="res-reverse-proxy" -q 2>/dev/null)" != "" ]]
+then
+    echo "Running res-reverse-proxy container found. Stopping it!"
+    docker stop res-reverse-proxy &>/dev/null
+fi
+
+if [[ "$(docker ps -f name="res-php" -q 2>/dev/null)" != "" ]]
+then
+    echo "Running res-php container found. Stopping it!"
+    docker stop res-php &>/dev/null
+fi
+
+if [[ "$(docker ps -f name="res-express" -q 2>/dev/null)" != "" ]]
+then
+    echo "Running res-express container found. Stopping it!"
+    docker stop res-express &>/dev/null
+fi
